a4512c104a9a6f935ccec422e42f9e15b5b4304d
Nimeshka Srimal <nimeshka@gmail.com>
2018-09-05 00:46:31 +0530

    Modified ClipDown.sh

--- clipDown.sh
+++ clipDown.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#Author: Nimeshka Srimal
+#Date/Time: 05 Sep 2018 12:45 AM
+#Description: Download one or more files from the URL copied to clipboard.
+
 while IFS= read -ra ADDR
 do
 	echo $ADDR
